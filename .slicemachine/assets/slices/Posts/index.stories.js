506a9de6e2a5547a6c805f3403bfe75097613cfc
Joilson M S Lopes <joilsonmirandasl@hotmail.com>
2022-08-15 22:27:39 -0300

    Add all components, services and config

---
+++ .slicemachine/assets/slices/Posts/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Posts';
+
+export default {
+  title: 'slices/Posts'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"posts","items":[],"primary":{"title":[{"type":"heading1","text":"Envisioneer end-to-end channels","spans":[]}],"description":[{"type":"paragraph","text":"Veniam excepteur cupidatat incididunt. Nostrud in est aute reprehenderit. Enim eiusmod nulla est aliqua.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
---
+++ src/components/ActiveLink/index.tsx
@@ -0,0 +1,21 @@
+import Link, { LinkProps } from 'next/link'
+import { useRouter } from 'next/router'
+import { ReactElement, cloneElement } from 'react'
+import styles from '../Header/styles.module.scss'
+
+interface ActiveLinkProps extends LinkProps {
+  children: ReactElement
+  activeClassName: string
+}
+
+export function ActiveLink({
+  children,
+  activeClassName,
+  ...rest
+}: ActiveLinkProps) {
+  const { asPath } = useRouter()
+
+  const className = rest.href === asPath ? activeClassName : ''
+
+  return <Link {...rest}>{cloneElement(children, { className })}</Link>
+}
